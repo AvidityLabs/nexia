df6d7fefed16aab3ae3846ee89d71ebbc55fe9a3
pmutua <pmutua@live.com>
2023-04-17 12:18:27 +0300

    deploy

--- core/settings.py
+++ core/settings.py
@@ -183,6 +183,9 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+DISABLE_COLLECTSTATIC = 0
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
