48befac928b754f871dd151a02aa1302703c968f
pmutua <pmutua@live.com>
2023-04-20 07:59:08 +0300

    fix: handle skip authorization for get_token and register paths

--- api/middlewares/authentication.py
+++ api/middlewares/authentication.py
@@ -19,31 +19,33 @@ def get_subscription(request):
 
 class RapidAPIAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
-        # Get the user's RapidAPI key from the request header
-        authorization_key = request.META.get('HTTP_AUTHORIZATION', None)
-        rapid_api_host = request.META.get('HTTP_X_RAPIDAPI_HOST', None)
-        rapid_api_proxy_secret = request.META.get('HTTP_X_RAPIDAPI_PROXY_SECRET', None)
+        if request.path == '/register/' or request.path== '/get_token/':
+            # Skip authentication for registration endpoint
+            return None
+        else:
+            # Get the user's RapidAPI key from the request header
+            authorization_key = request.META.get('HTTP_AUTHORIZATION', None)
+            rapid_api_host = request.META.get('HTTP_X_RAPIDAPI_HOST', None)
+            rapid_api_proxy_secret = request.META.get('HTTP_X_RAPIDAPI_PROXY_SECRET', None)
 
-        if not authorization_key:
-            raise AuthenticationFailed('Authorization key not found in request headers. Go to /register endpoint to get access key.')
-        if rapid_api_host:
-            raise AuthenticationFailed('RapidAPI Host not found in request headers')
-        if rapid_api_proxy_secret != HTTP_X_RAPIDAPI_PROXY_SECRET:
-            raise AuthenticationFailed(f'Invalid RapidAPI Proxy Secret. This API can only be accessed through the RapidAPI platform. Please sign up for RapidAPI and use their platform to access this API.{APP_URL}')
+            if not authorization_key:
+                raise AuthenticationFailed('Authorization key not found in request headers. Go to /register endpoint to get access key.')
+            if rapid_api_host:
+                raise AuthenticationFailed('RapidAPI Host not found in request headers')
+            if rapid_api_proxy_secret != HTTP_X_RAPIDAPI_PROXY_SECRET:
+                raise AuthenticationFailed(f'Invalid RapidAPI Proxy Secret. This API can only be accessed through the RapidAPI platform. Please sign up for RapidAPI and use their platform to access this API.{APP_URL}')
 
-        # Retrieve the user object
-        try:
-            user = User.objects.get(developer_id=authorization_key)
-            # Add subscription
-            subscription = get_subscription(request)
-            if user.subscription != subscription:
-                user.subscription = subscription
-                user.save()
-            return (user, None)
-        except User.DoesNotExist:
-            raise AuthenticationFailed('User not registered with this API. Please sign up for RapidAPI and use their platform to access this API.')
-
-
+            # Retrieve the user object
+            try:
+                user = User.objects.get(developer_id=authorization_key)
+                # Add subscription
+                subscription = get_subscription(request)
+                if user.subscription != subscription:
+                    user.subscription = subscription
+                    user.save()
+                return (user, None)
+            except User.DoesNotExist:
+                raise AuthenticationFailed('User not registered with this API. Please sign up for RapidAPI and use their platform to access this API.')
 
 
         
