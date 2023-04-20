adf8020a4c554cd5e134ec0f2f4474202ce41ac0
pmutua <pmutua@live.com>
2023-04-20 06:59:28 +0300

    refactor: authentication middleware

--- api/middlewares/authentication.py
+++ api/middlewares/authentication.py
@@ -21,34 +21,28 @@ def get_subscription(request):
 class RapidAPIAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         # Get the user's RapidAPI key from the request header
-        rapid_api_key = request.META.get('X_RAPID_API_KEY', None)
+        authorization_key = request.META.get('HTTP_AUTHORIZATION', None)
         rapid_api_host = request.META.get('HTTP_X_RAPIDAPI_HOST', None)
-        rapid_api_user =  request.META.get('HTTP_X_RAPIDAPI_USER', None)
-        print(request.META)
-        if not rapid_api_key:
-            raise AuthenticationFailed('RapidAPI key not found in request headers')
+
+        if not authorization_key:
+            raise AuthenticationFailed('Authorization key not found in request headers')
         if rapid_api_host:
             raise AuthenticationFailed('RapidAPI Host not found in request headers')
 
         # Retrieve the user object
         try:
-            user = User.objects.get(username=rapid_api_key)
+            user = User.objects.get(developer_id=authorization_key)
+            # Add subscription
+            subscription = get_subscription(request)
+            if user.subscription != subscription:
+                user.subscription = subscription
+                user.save()
+            return (user, None)
         except User.DoesNotExist:
-            # Create a new user if the user does not exist
-            user = User.objects.create_user(
-                first_name=rapid_api_user,
-                username=rapid_api_key,
-                email=f'{rapid_api_key}@nexia.user',
-                is_developer=True,
-                api_key=rapid_api_key
-            )
+            raise AuthenticationFailed('User not registered')
 
-        # Add subscription
-        subscription = get_subscription(request)
-        if user.subscription != subscription:
-            user.subscription = subscription
-            user.save()
 
-        return (user, None)
+
+        
 
         
