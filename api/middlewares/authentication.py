4acc4788dbe352b0611445b56aa59d9475de309f
pmutua <pmutua@live.com>
2023-04-20 07:16:20 +0300

    feat: update middleware for the api to be only be accessed on rapid api

--- api/middlewares/authentication.py
+++ api/middlewares/authentication.py
@@ -1,12 +1,11 @@
+import os
 import requests
 from rest_framework import authentication
 from rest_framework.exceptions import AuthenticationFailed
 from api.models import User, PricingPlan, Subscription
 
-
-
-
-
+HTTP_X_RAPIDAPI_PROXY_SECRET = os.environ.get('HTTP_X_RAPIDAPI_PROXY_SECRET')
+APP_URL = 'https://rapidapi.com/AvidityLabs/api/nexia2'
 
 def get_subscription(request):
     plan = request.META.get('HTTP_X_RAPIDAPI_SUBSCRIPTION')
@@ -23,11 +22,14 @@ class RapidAPIAuthentication(authentication.BaseAuthentication):
         # Get the user's RapidAPI key from the request header
         authorization_key = request.META.get('HTTP_AUTHORIZATION', None)
         rapid_api_host = request.META.get('HTTP_X_RAPIDAPI_HOST', None)
+        rapid_api_proxy_secret = request.META.get('HTTP_X_RAPIDAPI_PROXY_SECRET', None)
 
         if not authorization_key:
             raise AuthenticationFailed('Authorization key not found in request headers')
         if rapid_api_host:
             raise AuthenticationFailed('RapidAPI Host not found in request headers')
+        if rapid_api_proxy_secret != HTTP_X_RAPIDAPI_PROXY_SECRET:
+            raise AuthenticationFailed(f'Invalid RapidAPI Proxy Secret. This API can only be accessed through the RapidAPI platform. Please sign up for RapidAPI and use their platform to access this API.{APP_URL}')
 
         # Retrieve the user object
         try:
@@ -39,7 +41,8 @@ class RapidAPIAuthentication(authentication.BaseAuthentication):
                 user.save()
             return (user, None)
         except User.DoesNotExist:
-            raise AuthenticationFailed('User not registered')
+            raise AuthenticationFailed('User not registered with this API. Please sign up for RapidAPI and use their platform to access this API.')
+
 
 
 
