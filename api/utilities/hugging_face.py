7246f7a3a2e1627789eb7724d62447a5f52c95e9
pmutua <pmutua@live.com>
2023-04-25 00:55:54 +0300

    update new changes

--- api/utilities/hugging_face.py
+++ api/utilities/hugging_face.py
@@ -11,14 +11,20 @@ SENTIMENT_MODEL_TOKEN=os.environ.get('SENTIMENT_MODEL_TOKEN')
 # https://huggingface.co/j-hartmann/emotion-english-distilroberta-base
 def query_emotions_model(payload):
 	headers = {"Authorization": f"Bearer {EMOTION_MODEL_TOKEN}"}
-	response = requests.post(EMOTION_MODEL_URL, headers=headers, json=payload)
-	return response.json()
+	try:
+		response = requests.post(EMOTION_MODEL_URL, headers=headers, json=payload)
+		return response.json()
+	except Exception as e:
+		return None
 
 # https://huggingface.co/cardiffnlp/twitter-roberta-base-sentiment
 def query_sentiment_model(payload):
 	headers = {"Authorization": f"Bearer {SENTIMENT_MODEL_TOKEN}"}
-	response = requests.post(SENTIMENT_MODEL_URL, headers=headers, json=payload)
-	return response.json()
+	try:
+		response = requests.post(SENTIMENT_MODEL_URL, headers=headers, json=payload)
+		return response.json()
+	except Exception as e:
+		return None
 	
 
 		
