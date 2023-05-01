bdb268c19d34b8e984f8b3014982b87df6d756da
pmutua <pmutua@live.com>
2023-05-01 03:05:07 +0300

    update changes

--- core/settings.py
+++ core/settings.py
@@ -166,7 +166,7 @@ CACHE_TTL = 60 * 15
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": os.environ.get('REDIS_URL'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
