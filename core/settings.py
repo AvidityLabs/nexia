991ed01f0bf93c5999b4f9f2a84e436a2d283d88
pmutua <pmutua@live.com>
2023-06-01 01:28:16 +0300

    try to add settings DISABLE_COLLECTSTATIC=0

--- core/settings.py
+++ core/settings.py
@@ -237,7 +237,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+DISABLE_COLLECTSTATIC=0
 STATIC_URL = "/static/"
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
