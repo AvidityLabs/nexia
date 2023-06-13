90a8fbed3921a7f699b7bf4512607ca2002039d8
pmutua <pmutua@live.com>
2023-06-13 19:06:41 +0300

    fix: remove check to see if postgres has started

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-echo "Waiting for PostgreSQL..."
+# echo "Waiting for PostgreSQL..."
 
-while ! nc -z "$DB_HOST" "$DB_PORT"; do
-    sleep 0.1
-done
+# while ! nc -z "$DB_HOST" "$DB_PORT"; do
+#     sleep 0.1
+# done
 
-echo "PostgreSQL started"
+# echo "PostgreSQL started"
 
 
 # Read the contents of pip-installation.log
