8f58917b06737164f70c6d6c1c7f38a240f63c8d
pmutua <pmutua@live.com>
2023-06-14 21:37:34 +0300

    ci: change command to create super user

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@
 # python manage.py flush --no-input --settings=core.settings.dev
 python manage.py makemigrations --settings=core.settings.dev
 python manage.py migrate --settings=core.settings.dev
-python manage.py createsuperuser --username=admin --email=admin@example.com --password=123# --noinput --settings=core.settings.dev
+python manage.py createsuperuser --noinput --settings=core.settings.dev
 # Check if migrations are needed
 # python manage.py makemigrations --dry-run --check --settings=core.settings.dev > /dev/null 2>&1 MIGRATIONS_NEEDED=$?
 
