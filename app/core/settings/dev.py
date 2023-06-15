61afb6d04132ecd47c059901bc4575ad48e00545
pmutua <pmutua@live.com>
2023-06-15 22:14:11 +0300

    chore: add localhost:4200 in cors whitelist

--- app/core/settings/dev.py
+++ app/core/settings/dev.py
@@ -12,6 +12,7 @@ PAYPAL_TEST = True
 # CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
     'https://andika.pro',
+    'https://localhost:4200'
 ]
 
 CORS_ALLOW_METHODS = [
--- docker/dev/python/entrypoint.dev.sh
+++ docker/dev/python/entrypoint.dev.sh
@@ -14,30 +14,30 @@
 # Read the contents of pip-installation.log
 # cat pip-installation.log
 
-python manage.py flush --no-input --settings=core.settings.dev
+python manage.py runserver --no-input --settings=core.settings.dev
 
-# Check if migrations are needed
-python manage.py makemigrations --dry-run --check --settings=core.settings.dev > /dev/null 2>&1
-MIGRATIONS_NEEDED=$?
+# # Check if migrations are needed
+# python manage.py makemigrations --dry-run --check --settings=core.settings.dev > /dev/null 2>&1
+# MIGRATIONS_NEEDED=$?
 
-if [ $MIGRATIONS_NEEDED -eq 0 ]; then
-    # Migrations needed, apply them
-    python manage.py makemigrations --settings=core.settings.dev
-    python manage.py migrate --settings=core.settings.dev
-fi
+# if [ $MIGRATIONS_NEEDED -eq 0 ]; then
+#     # Migrations needed, apply them
+#     python manage.py makemigrations --settings=core.settings.dev
+#     python manage.py migrate --settings=core.settings.dev
+# fi
 
-# Check if superuser needs to be created
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists()" --settings=core.settings.dev | grep "False" > /dev/null 2>&1
-SUPERUSER_EXISTS=$?
+# # Check if superuser needs to be created
+# python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists()" --settings=core.settings.dev | grep "False" > /dev/null 2>&1
+# SUPERUSER_EXISTS=$?
 
-if [ $SUPERUSER_EXISTS -eq 0 ]; then
-    # Superuser does not exist, create it
-    echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')" | python manage.py shell --settings=core.settings.dev
-fi
+# if [ $SUPERUSER_EXISTS -eq 0 ]; then
+#     # Superuser does not exist, create it
+#     echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')" | python manage.py shell --settings=core.settings.dev
+# fi
 
-# Start the Gunicorn server
-gunicorn core.wsgi:application --bind 0.0.0.0:8000 --env DJANGO_SETTINGS_MODULE=core.settings.dev
+# # Start the Gunicorn server
+# gunicorn core.wsgi:application --bind 0.0.0.0:8000 --env DJANGO_SETTINGS_MODULE=core.settings.dev
 
-# unlink /etc/nginx/sites-enabled/default
+# # unlink /etc/nginx/sites-enabled/default
 
-# nginx -g  'daemon off;'
+# # nginx -g  'daemon off;'
